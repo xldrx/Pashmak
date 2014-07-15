52433da2b21984cae520875423299e2d2bc7f9ac
Sayed Hadi Hashemi <info@xldrx.com>
2014-07-15 00:57:42 -0500

    Fix problems

--- iPhone-gui/pashmak-gui/xlMain.mm
+++ iPhone-gui/pashmak-gui/xlMain.mm
@@ -8,11 +8,33 @@
 
 #import "xlMain.h"
 
+#include "Constants.h"
+#include "Engine.h"
+#include "FileManager.h"
+#include <iostream>
+#include <string>
+
+
+//#import "../../core/Pashmak/main.cpp"
+
 @interface xlMain ()
 
 @end
 
 @implementation xlMain
+
+- (IBAction)click:(id)sender {
+    FileManager myFileManager;
+	Engine myEngine;
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Lenna" ofType:@"png"];
+    auto cpath = {(std::string)[path UTF8String]};
+    
+	auto media = myFileManager.LoadFiles(cpath);
+	auto output = myEngine.CreateZoe(media, Themes::Oldie);
+	myFileManager.SaveFile(output, "./test.png");
+
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
