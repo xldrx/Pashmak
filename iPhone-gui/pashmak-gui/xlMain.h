27916715d0aae1299719cb4960d95d7ee0267a56
Sayed Hadi Hashemi <info@xldrx.com>
2014-07-15 00:15:37 -0500

    Fix initial problems.

---
+++ iPhone-gui/pashmak-gui/xlMain.h
@@ -0,0 +1,13 @@
+//
+//  xlMain.h
+//  pashmak-gui
+//
+//  Created by Sayed Hadi Hashemi on 7/15/14.
+//  Copyright (c) 2014 Sayed Hadi Hashemi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xlMain : UIViewController
+
+@end
